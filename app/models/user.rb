78cb0bb25cf5e4dca820c2ee252729f9fbebd818
kotaro0217 <kotaro96480321@gamil.com>
2020-12-30 20:09:21 +0900

    ユーザー管理機能の修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,7 @@ class User < ApplicationRecord
 
          validates :nickname, presence: true
 
-      with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数を使用してください' } do
-         validates :password
-      end
+         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数を使用してください' }
 
       with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
          validates :first_name
