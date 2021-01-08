5860179509212024ab4b34974483af99b1a533b3
kotaro0217 <kotaro96480321@gamil.com>
2021-01-08 21:19:47 +0900

    モデルの単体テストコード

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create] 
+#ログインしていなければ指定ページにとべない
   def index
     @items = "index"
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :image, :value, :condition_id, :category_id, :postage_id, :prefecture_id, :shipment_day_id).merge(user_id: current_user.id)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品の出品' do
+    context '商品出品がうまくいくとき' do
+      it "出品画像、商品名、商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、販売価格が存在すれば登録できること" do
+        expect(@item).to be_valid
+      end
+    end
+    
+    context '商品出品がうまくいかないとき' do
+      it "画像が１枚ないと登録できない" do
+        @item.image = nil
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("出品画像を入力してください")
+      end
+      
+      it "商品名がないと登録できない" do
+        @item.name = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
+      end
+
+      it "カテゴリーを選択していないと登録できない" do
+        @item.category_id = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("カテゴリーは空欄以外を選択してください")
+      end
+
+      it "商品の状態を選択していないと登録できない" do
+        @item.condition_id = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("商品の状態は空欄以外を選択してください")
+      end
+
+      it "配送料の負担を選択していないと登録できない" do
+        @item.postage_id = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("配送料の負担は空欄以外を選択してください")
+      end
+
+      it "発送元の地域を選択していないと登録できない" do
+        @item.prefecture_id = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("発送元の地域は空欄以外を選択してください")
+      end
+
+      it "発送までの日数を選択していないと登録できない" do
+        @item.shipment_day_id = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("発送までの日数は空欄以外を選択してください")
+      end
+
+      it "価格を入力していないと登録できない" do
+        @item.value = ""
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("販売価格を入力してください")
+      end
+    end
+  end
 end
