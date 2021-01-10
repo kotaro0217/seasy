fce735f7e333d58ce0d0f59ffea5752a27b22152
kotaro0217 <kotaro96480321@gamil.com>
2021-01-10 16:10:00 +0900

    販売手数料・販売利益の実装

--- app/javascript/item_value.js
+++ app/javascript/item_value.js
@@ -1,3 +1,15 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+function calc() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1)); //Math. floor→数値の小数点以下の切り捨て処理を行う
+    console.log(addTaxDom);
+    const profit = document.getElementById("profit")
+    profit.innerHTML =(Math.floor(inputValue - addTaxDom.innerHTML)); //価格-販売手数料で利益を出す
+    console.log(profit);
+  });
+}
+window.addEventListener("load",calc)
