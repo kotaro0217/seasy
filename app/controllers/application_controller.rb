888688c4d3313a274097c2d59761504683a11c10
kotaro0217 <kotaro96480321@gamil.com>
2021-01-12 12:57:00 +0900

    before_action :authenticate_user!の定義の見直し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,10 @@
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :move_to_index, except: [:index, :new, :create ]
 
    private
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :email, :password, :first_name, :family_name, :read_first_name, :read_family_name, :birth]
     )
-  end
-  
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create] 
+  before_action :move_to_index, except: [:index, :new, :create ]
   
 #ログインしていなければ指定ページにとべない
   def index
@@ -22,5 +23,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :text, :image, :value, :condition_id, :category_id, :postage_id, :prefecture_id, :shipment_day_id).merge(user_id: current_user.id)
+  end
+    
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
