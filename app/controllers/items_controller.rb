c3b446e536a3c793d3fb3360a221faffb0397d78
kotaro0217 <kotaro96480321@gamil.com>
2021-01-18 00:03:32 +0900

    編集ページに遷移する際の条件の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :selectitem, only: [:show, :edit, :update ]
 
   # ログインしていなければ指定ページにとべない
   def index
@@ -20,22 +21,22 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
+    else
+      render :edit
+    end
   end
-
 
   # def destroy
     # @item = Item.find(params[:id])
   # end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
@@ -43,4 +44,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :image, :value, :condition_id, :category_id, :postage_id, :prefecture_id, :shipment_day_id).merge(user_id: current_user.id)
   end
+
+  def selectitem
+    @item = Item.find(params[:id])
+  end
 end
+
