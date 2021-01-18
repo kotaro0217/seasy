78b752471a522b7419ed90bf06f8ab1dde08d27c
kotaro0217 <kotaro96480321@gamil.com>
2021-01-18 21:51:24 +0900

    destroyのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :selectitem, only: [:show, :edit, :update]
+  before_action :selectitem, only: [:show, :edit, :update, :destroy]
 
   # ログインしていなければ指定ページにとべない
   def index
@@ -32,9 +32,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # @item = Item.find(params[:id])
-  # end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   def show
   end
