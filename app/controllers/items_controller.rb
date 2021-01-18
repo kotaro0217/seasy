2c8d1f6b38f96106a1c12807472420b528136eb3
kotaro0217 <kotaro96480321@gamil.com>
2021-01-18 22:36:28 +0900

    item_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    redirect_to root_path if current_user.id != @item.user_id
     @item.destroy
     redirect_to root_path
   end
