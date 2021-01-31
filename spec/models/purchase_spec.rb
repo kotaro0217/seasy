222296015b61ee9161f11003143b059a6c8974f5
kotaro0217 <kotaro96480321@gamil.com>
2021-01-31 12:43:51 +0900

    不要ファイルの削除

---
+++ spec/models/purchase_spec.rb
@@ -0,0 +1,14 @@
+# require 'rails_helper'
+# 
+# 
+# 
+  # it "valueがあれば保存ができること" do
+    # expect(@purchase).to be_valid
+  # end
+# 
+  # it "valueが空では保存ができないこと" do
+    # @purchase.value = nil
+    # @purchase.valid?
+    # expect(@purchase.errors.full_messages).to include("value can't be blank")
+  # end
+# end
