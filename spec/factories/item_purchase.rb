cb80c2e01e7d9adc59abfefe02f0a67f7053e55e
kotaro0217 <kotaro96480321@gamil.com>
2021-01-31 19:40:10 +0900

    factorybotの設定

---
+++ spec/factories/item_purchase.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :item_purchase do
+    association :user 
+    association :item
+
+    # purchase_id
+    postcode          { '000-0000' }
+    prefecture_id     { 2 }
+    city              {'市町村'}
+    block             {'番地'}
+    building          {'建物名'}
+    phone             { '00000000000' }
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
