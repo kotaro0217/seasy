9781b1c9baeaf891d02a2fbf83e2875758020ba4
kotaro0217 <kotaro96480321@gamil.com>
2021-02-01 14:41:10 +0900

    商品購入後は購入ページに遷移させない

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,22 +1,18 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
     @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
-  end
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase != nil 
+    end
 
   def create
     @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new(purchase_params)
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp::Charge.create(
-      amount: @item[:value],
-      card: purchase_params[:token],
-      currency: 'jpy'
-    )
     if @item_purchase.valid?
       @item_purchase.save
+      pay_item
       redirect_to root_path
     else
       @item = Item.find(params[:item_id])
@@ -31,4 +27,13 @@ class PurchasesController < ApplicationController
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item[:value],
+      card: purchase_params[:token],
+      currency: 'jpy'
+      )
+end
 end
